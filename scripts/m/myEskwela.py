bd5415e2adb67a8aa3d5dc49de8a9c74977f5417
Regina Mae Moso <maellanes@gmail.com>
2012-09-23 13:29:45 +0800

    salt in auth
    kieth, novo pls recheck :)

--- scripts/m/myEskwela.py
+++ scripts/m/myEskwela.py
@@ -1,4 +1,5 @@
 import pg, string, os
+import smtplib
 
 class Auth:
 
@@ -9,25 +10,35 @@ class Auth:
 
     #Auth Checker...
     def auth(self, db):
-        q0 = "SELECT login('"+self.username+"', '"+self.password+"')";
-        query = db.query(q0); result = query.dictresult();
+
+		q1 = "SELECT getsalt('"+self.username +")"
         
-        if (result[0]['login'] == "TRUE"):
+        query2 = db.query(q1); salt = quesry2.dictresult()
+		hash = encryptPass(salt, self.password)
+		
+		q0 = "SELECT login('"+self.username+"', '"+hash+"')" #edit login to accept hash instead of password
+		query = db.query(q0); result = query.dictresult()
+        
+		if (result[0]['login'] == "TRUE"):
             return True
         else:
             return False
-
+			
+	def encryptPass(salt, pwd):
+	hashPass = hashlib.sha512(pwd + salt).hexdigest()
+	return hashPass
+			
     #Getting User Role
     def get_user_role_from_username(self, db):
         q0 = "SELECT account_role('"+self.username+"')"
-        query = db.query(q0); result = query.dictresult();
+        query = db.query(q0); result = query.dictresult()
 
         return result[0]['account_role']
 
     #Getting ID Number...
     def get_id_number_from_username(self, db):
         q0 = "SELECT account_role_id('"+self.username+"')"
-        query = db.query(q0); result = query.dictresult();
+        query = db.query(q0); result = query.dictresult()
 
         return result[0]['account_role_id']
 
@@ -41,6 +52,24 @@ class Auth:
         
         return string
 
+class Email:
+
+	def __init__(self):
+		print "foo"
+	
+	def sendemail(from_addr, to_addr_list, cc_addr_list, subject, message, login, password, smtpserver='smtp.gmail.com:587'):
+		header  = 'From: %s\n' % from_addr
+		header += 'To: %s\n' % ','.join(to_addr_list)
+		header += 'Cc: %s\n' % ','.join(cc_addr_list)
+		header += 'Subject: %s\n\n' % subject
+		message = header + message
+ 
+		server = smtplib.SMTP(smtpserver)
+		server.starttls()
+		server.login(login,password)
+		problems = server.sendmail(from_addr, to_addr_list, message)
+		server.quit()
+		return problems		
 
 class Database:
 
